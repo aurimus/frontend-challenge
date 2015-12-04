48d5aaf635c71059ce569ee3a251ad1cf62a377c
Aurimas K <4urimas@gmail.com>
2015-12-04 16:14:49 +0100

    Initial dumb implementation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
+// Hide the text
+let $read_more_text = document.querySelector('.read_more_text');
+    $read_more_text.style.display = 'none';
+
+// Show the button
+let $read_more_control = document.querySelector('.read_more_control')
+    $read_more_control.style.display = 'block';
+
+
+/** Enable control **/
+
+let $read_more_button = document.querySelector('.read_more_control button');
+$read_more_button.addEventListener('click', function (evt) {
+    if ($read_more_text.style.display == 'none'){
+        $read_more_text.style.display = 'block';
+        this.textContent = 'Show less';
+    } else {
+        $read_more_text.style.display = 'none';
+        this.textContent = 'Show more';
+    }
+});
+
+
 console.log('ready!');
